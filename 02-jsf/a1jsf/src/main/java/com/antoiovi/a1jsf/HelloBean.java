f9f1464153c419c4938d5c874629fb4d5d33fc35
antoiovi <antoiovi@antoiovi.com>
2018-09-10 13:57:13 +0200

    Aggiunto a1jsf

---
+++ 02-jsf/a1jsf/src/main/java/com/antoiovi/a1jsf/HelloBean.java
@@ -0,0 +1,24 @@
+package com.antoiovi.a1jsf;
+
+
+public class HelloBean implements java.io.Serializable{
+
+
+	String message;
+
+
+	public HelloBean(){
+		message="Buon giorno";
+	}
+
+
+	public void setMessage(String message)
+{
+		 this.message=message;
+	}
+
+	public String getMessage(){
+
+return message;
+	}
+}
