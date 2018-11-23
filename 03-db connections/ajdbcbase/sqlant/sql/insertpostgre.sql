60753944a03fd843583f7e9e8722b52af664e830
antoiovi <antoiovi@antoiovi.com>
2018-11-23 19:03:46 +0100

    aggiunto override-web.xml

---
+++ 03-db connections/ajdbcbase/sqlant/sql/insertpostgre.sql
@@ -0,0 +1,2 @@
+INSERT INTO antoiovi.tutorials.persone (username,nome,cognome,email,password) VALUES ('ant01','Antos','Bellis','antp@ader.it','password1');
+INSERT INTO antoiovi.tutorials.persone (username,nome,cognome,email,password) VALUES ('ant02','Antos','BellOCCIO','antJKAp@ader.it','password2'); 
